64e35f41ba8f62b05d984b74c2fb352da933cab3
Benjamin Oakes <boakes@spacebarlabs.com>
2026-05-26 13:58:14 -0500

    Remove content

--- scripts/download-styles.bash
+++
@@ -1,2 +0,0 @@
-wget 'https://jenil.github.io/bulmaswatch/lux/bulmaswatch.min.css' -O 'styles/lux-bulmaswatch.min.css'
-wget 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' -O 'styles/fontawesome-6.5.1-all.min.css'
